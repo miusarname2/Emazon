a97546861f92a9368c91f68b5f2c4159923a24d2
miusarname2 <oscarmauricio631@gmail.com>
2024-08-22 08:37:35 -0500

    feat: :sparkles: Added more validations and error handling

--- src/main/java/com/pragma/Emazon/infrastructure/output/jpa/repository/ICategoriaRepository.java
+++ src/main/java/com/pragma/Emazon/infrastructure/output/jpa/repository/ICategoriaRepository.java
@@ -4,4 +4,5 @@ import com.pragma.Emazon.infrastructure.output.jpa.entity.CategoriaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ICategoriaRepository extends JpaRepository<CategoriaEntity,Long> {
+    boolean existsByNombre(String nombre);
 }
