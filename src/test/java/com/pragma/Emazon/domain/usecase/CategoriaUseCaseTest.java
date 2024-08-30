60e79e50f6538ec385555801e119796b23396c0e
miusarname2 <oscarmauricio631@gmail.com>
2024-08-30 09:13:28 -0500

    test: :white_check_mark: The tests were modified

--- src/test/java/com/pragma/Emazon/domain/usecase/CategoriaUseCaseTest.java
+++ src/test/java/com/pragma/Emazon/domain/usecase/CategoriaUseCaseTest.java
@@ -1,5 +1,11 @@
 package com.pragma.Emazon.domain.usecase;
 
+import com.pragma.Emazon.application.dto.CategoriaRequest;
+import com.pragma.Emazon.application.dto.CategoriaResponse;
+import com.pragma.Emazon.application.handler.CategoriaHandler;
+import com.pragma.Emazon.application.mapper.CategoriaRequestMapper;
+import com.pragma.Emazon.application.mapper.CategoriaResponseMapper;
+import com.pragma.Emazon.domain.api.ICategoriaPortService;
 import com.pragma.Emazon.domain.model.Categoria;
 import com.pragma.Emazon.domain.spi.ICategoriaPersistence;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,10 +24,16 @@ import static org.mockito.Mockito.when;
 class CategoriaUseCaseTest {
 
     @Mock
-    private ICategoriaPersistence categoriaPersistence;
+    private ICategoriaPortService categoriaPortService;
+
+    @Mock
+    private CategoriaRequestMapper categoriaRequestMapper;
+
+    @Mock
+    private CategoriaResponseMapper categoriaResponseMapper;
 
     @InjectMocks
-    private CategoriaUseCase categoriaUseCase;
+    private CategoriaHandler categoriaHandler;
 
     @BeforeEach
     void setUp() {
@@ -31,19 +43,26 @@ class CategoriaUseCaseTest {
     @Test
     void saveCategoria() {
         // Arrange
+        CategoriaRequest categoriaRequest = new CategoriaRequest();
+        categoriaRequest.setNombre("Electrónica");
+        categoriaRequest.setDescripcion("Productos electrónicos");
+
         Categoria categoria = new Categoria();
         categoria.setNombre("Electrónica");
         categoria.setDescripcion("Productos electrónicos");
 
+        when(categoriaRequestMapper.toCategoria(categoriaRequest)).thenReturn(categoria);
+
         // Act
-        categoriaUseCase.saveCategoria(categoria);
+        categoriaHandler.saveCategoria(categoriaRequest);
 
         // Assert
-        verify(categoriaPersistence).saveCategoria(categoria);
+        verify(categoriaRequestMapper).toCategoria(categoriaRequest);
+        verify(categoriaPortService).saveCategoria(categoria);
     }
 
     @Test
-    void listCategorias() {
+    void listCategoria() {
         // Arrange
         Categoria categoria1 = new Categoria();
         categoria1.setNombre("Electrónica");
@@ -55,14 +74,27 @@ class CategoriaUseCaseTest {
 
         List<Categoria> categorias = Arrays.asList(categoria1, categoria2);
 
-        when(categoriaPersistence.listCategorias()).thenReturn(categorias);
+        CategoriaResponse categoriaResponse1 = new CategoriaResponse();
+        categoriaResponse1.setNombre("Electrónica");
+        categoriaResponse1.setDescripcion("Productos electrónicos");
+
+        CategoriaResponse categoriaResponse2 = new CategoriaResponse();
+        categoriaResponse2.setNombre("Hogar");
+        categoriaResponse2.setDescripcion("Productos para el hogar");
+
+        List<CategoriaResponse> categoriaResponses = Arrays.asList(categoriaResponse1, categoriaResponse2);
+
+        // Configura el comportamiento del mock con los parámetros esperados
+        when(categoriaPortService.listCategorias("nombre", true, 0, 10)).thenReturn(categorias);
+        when(categoriaResponseMapper.toResponseList(categorias)).thenReturn(categoriaResponses);
 
         // Act
-        List<Categoria> result = categoriaUseCase.listCategorias();
+        List<CategoriaResponse> result = categoriaHandler.listCategoria("nombre", true, 0, 10);
 
         // Assert
-        verify(categoriaPersistence).listCategorias();
-        assertEquals(categorias, result);
+        verify(categoriaPortService).listCategorias("nombre", true, 0, 10);
+        verify(categoriaResponseMapper).toResponseList(categorias);
+        assertEquals(categoriaResponses, result);
     }
 
 }
