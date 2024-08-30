fe1795a9ce7dcd2eaed77e563cf7bd00b55d3089
miusarname2 <oscarmauricio631@gmail.com>
2024-08-30 14:47:11 -0500

    feat: :twisted_rightwards_arrows: Merge branch 'feature/H03' into develop

--- src/main/java/com/pragma/Emazon/application/handler/ICategoriaHandler.java
+++ src/main/java/com/pragma/Emazon/application/handler/ICategoriaHandler.java
@@ -10,10 +10,6 @@ public interface ICategoriaHandler {
 
     void saveCategoria(CategoriaRequest categoriaRequest);
 
-<<<<<<< HEAD
     List<CategoriaResponse> listCategoria(String sortBy, boolean ascending,int page, int size);
-=======
-    List<CategoriaResponse> listCategoria();
->>>>>>> feature/H03
 
 }
