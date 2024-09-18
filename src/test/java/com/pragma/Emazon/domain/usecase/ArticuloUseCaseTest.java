fe1fe153a9d3662da71c32baf7b8558c9ceb51b8
miusarname2 <oscarmauricio631@gmail.com>
2024-09-18 14:52:58 -0500

    feat: :sparkles: The necessary tests were carried out

--- src/test/java/com/pragma/Emazon/domain/usecase/ArticuloUseCaseTest.java
+++ src/test/java/com/pragma/Emazon/domain/usecase/ArticuloUseCaseTest.java
@@ -8,6 +8,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
@@ -46,4 +49,65 @@ class ArticuloUseCaseTest {
         assertEquals(articulo, result);
     }
 
+    @Test
+    void listArticulos() {
+        // Arrange
+        Articulo articulo1 = new Articulo();
+        articulo1.setId(1L);
+        articulo1.setNombre("Articulo 1");
+        articulo1.setCantidad(10);
+        articulo1.setPrecio(100.0);
+        articulo1.setIdCategoria(1L);
+        articulo1.setIdMarca(1L);
+
+        Articulo articulo2 = new Articulo();
+        articulo2.setId(2L);
+        articulo2.setNombre("Articulo 2");
+        articulo2.setCantidad(20);
+        articulo2.setPrecio(200.0);
+        articulo2.setIdCategoria(1L);
+        articulo2.setIdMarca(1L);
+
+        List<Articulo> expectedArticulos = Arrays.asList(articulo1, articulo2);
+
+        String sortBy = "nombre";
+        boolean ascending = true;
+        int page = 0;
+        int size = 10;
+
+        // Mock the behavior of articuloPersistence
+        when(articuloPersistence.listCategorias(sortBy, ascending, page, size)).thenReturn(expectedArticulos);
+
+        // Act
+        List<Articulo> result = articuloUseCase.listArticulos(sortBy, ascending, page, size);
+
+        // Assert
+        verify(articuloPersistence).listCategorias(sortBy, ascending, page, size);
+        assertEquals(expectedArticulos, result);
+    }
+
+    @Test
+    void agregarArticuloAlStock() {
+        // Arrange
+        String nombreArticulo = "Articulo 1";
+        int cantidadAAgregar = 5;
+
+        Articulo articuloExistente = new Articulo();
+        articuloExistente.setCantidad(10);
+
+        Articulo articuloActualizado = new Articulo();
+        articuloActualizado.setCantidad(15); // 10 + 5
+
+        // Mock the behavior of articuloPersistence
+        when(articuloPersistence.obtenerArticulo(nombreArticulo)).thenReturn(articuloExistente);
+        when(articuloPersistence.agregarArticuloAlStock(articuloExistente)).thenReturn(articuloActualizado);
+
+        // Act
+        Articulo result = articuloUseCase.agregarArticuloAlStock(nombreArticulo, cantidadAAgregar);
+
+        // Assert
+        verify(articuloPersistence).obtenerArticulo(nombreArticulo);
+        verify(articuloPersistence).agregarArticuloAlStock(articuloExistente);
+        assertEquals(articuloActualizado, result);
+    }
 }
