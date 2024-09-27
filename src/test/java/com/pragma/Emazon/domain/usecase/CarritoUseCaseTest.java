c68d819d24463511faa31290093cc6f223cd04b3
miusarname2 <oscarmauricio631@gmail.com>
2024-09-27 11:52:30 -0500

    test: :white_check_mark: The necessary tests were added

--- src/test/java/com/pragma/Emazon/domain/usecase/CarritoUseCaseTest.java
+++ src/test/java/com/pragma/Emazon/domain/usecase/CarritoUseCaseTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -74,5 +77,38 @@ class CarritoUseCaseTest {
         verify(carritoPersistence).deleteCarrito(carrito);
         assertEquals(carrito, result);
     }
+
+    @Test
+    void getAllCarrito() {
+        // Arrange
+        String sortBy = "id";
+        boolean ascending = true;
+        int page = 0;
+        int size = 10;
+        Long id = 1L;
+
+        List<Carrito> expectedCarritoList = new ArrayList<>();
+        Carrito carrito1 = new Carrito();
+        carrito1.setId(1L);
+        carrito1.setCantidad(50.0);
+
+        Carrito carrito2 = new Carrito();
+        carrito2.setId(2L);
+        carrito2.setCantidad(25.0);
+
+        expectedCarritoList.add(carrito1);
+        expectedCarritoList.add(carrito2);
+
+        // Mock the behavior of carritoPersistence
+        when(carritoPersistence.GetAllCarrito(sortBy, ascending, page, size, id)).thenReturn(expectedCarritoList);
+
+        // Act
+        List<Carrito> result = carritoUseCase.GetAllCarrito(sortBy, ascending, page, size, id);
+
+        // Assert
+        verify(carritoPersistence).GetAllCarrito(sortBy, ascending, page, size, id);
+        assertEquals(expectedCarritoList, result);
+    }
+
 
 }
